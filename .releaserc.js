b12159149fe45b5fef0d28212d3c279480bc6ab4
Christian Frenzl <chriseff@users.noreply.github.com>
2022-01-02 18:52:58 +0100

    style: fix code style

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,5 @@
 module.exports = {
-	branches: [
-		'main',
-		{ name: 'alpha', prerelease: true },
-		{ name: 'beta', prerelease: true },
-	],
+	branches: ['main', { name: 'alpha', prerelease: true }, { name: 'beta', prerelease: true }],
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
